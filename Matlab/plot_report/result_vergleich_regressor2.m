15d0a970d69aadc10172af3edf807f2b336348b2
Yi Cui <cuiyicuiyi2008@gmail.com>
2020-06-02 23:41:18 +0200

    update

--- Matlab/plot_report/result_vergleich_regressor2.m
+++ Matlab/plot_report/result_vergleich_regressor2.m
@@ -2,22 +2,12 @@ clear all
 close all
 
 %% Import data set (predict und target)
-result_layer_1 = readtable('version_4/regressor_layer_1.csv');
-result_layer_2 = readtable('version_4/regressor_layer_2.csv');
-result_layer_3 = readtable('version_4/regressor_layer_3.csv');
-target =  readtable('version_4/regressor_target.csv');
-
-result_layer_1 = table2array(result_layer_1);
-result_layer_1(1,:) = [];
-
-result_layer_2 = table2array(result_layer_2);
-result_layer_2(1,:) = [];
+result_layer_3 = readtable('version_6P1/regressor_layer_3.csv');
+target =  readtable('version_6P1/regressor_target.csv');
 
 result_layer_3 = table2array(result_layer_3);
 result_layer_3(1,:) = [];
 
-result_layer(1,:,:) = result_layer_1;
-result_layer(2,:,:) = result_layer_2;
 result_layer(3,:,:) = result_layer_3;
 
 target = table2array(target);
@@ -25,10 +15,12 @@ target(1,:) = [];
 
 %% plot
 label = ["m2","m3","m4","k","alpha","beta"];
+
 for i = 3
     figure(i)
     for j=1:6
         subplot(3,2,j)
+%         result_layer(i, result_layer(i,:,j+1)<= 1.03 & result_layer(i,:,j+1) >= 0.97, j+1) = 1;
         scatter(target(:,j+1), result_layer(i,:,j+1))
         grid on
         hold on
@@ -41,19 +33,22 @@ for i = 3
         title(label(1,j))
     end
 end
-sgtitle('ideale Datenerfassung')
+% sgtitle('version_6P1 1P')
+sgtitle('gerundete 1P  Daten in 7P Modell')
 
 for i = 3
     figure(3+i)
     for j=1:6
         subplot(3,2,j)
-        scatter(target(1:300,1),target(1:300,j+1),'*')
+        scatter(target(1:200,1),target(1:200,j+1),'*')
         grid on
         hold on
-        scatter(result_layer(i,1:300,1),result_layer(i,1:300,j+1))
+        scatter(result_layer(i,1:200,1),result_layer(i,1:200,j+1))
         xlabel('examples')
         ylabel(label(1,j))
         legend('true', 'predict')
     end
 end
-sgtitle('ideale Datenerfassung')
+sgtitle('gerundete 1P Daten in 7P Modell')
+
+
